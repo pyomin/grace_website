a590a3240d9144f048c6a5f8244781cd5ee24349
naerim <naerim1119@gmail.com>
2020-08-15 00:31:28 +0900

    [ADD] main.html

--- grace/views.py
+++ grace/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Recommend
 
 # Create your views here.
+
+
+def index(request):
+    recommends = Recommend.objects.all()
+    return render(
+        request,
+        'grace/main.html',
+        {
+            'recommends': recommends,
+        }
+    )
